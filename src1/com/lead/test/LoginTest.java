43a45347639cf7cdfab84f793ad56a23f3f3561a
Venu Tiwari <venus.jun25@gmail.com>
2021-10-04 22:05:18 -0600

    Update LoginTest.java

--- src1/com/lead/test/LoginTest.java
+++ src1/com/lead/test/LoginTest.java
@@ -11,7 +11,7 @@ import com.lead.pages.DsLoginPage;
  
 		DsLoginPage  loginpage = PageFactory.initElements(driver, DsLoginPage.class);
 		loginpage.setUser("venus@drivesolution.ca");
-		loginpage.setPassword("!!D#6!N*");
+		loginpage.setPassword(");
 		loginpage.clickOnLoginButton();
 		
 	driver.navigate().to("https://drivesolutions-dev-ed.lightning.force.com/lightning/page/home");
